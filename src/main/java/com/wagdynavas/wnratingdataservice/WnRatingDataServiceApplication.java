1ae189d12981c69ddcf46b1c3cac4cf34e5b1f4f
Dinho <dnavas@sdd.com.ar>
2019-09-22 12:05:31 -0300

    first commit wn.rating-data-service

---
+++ src/main/java/com/wagdynavas/wnratingdataservice/WnRatingDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wagdynavas.wnratingdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WnRatingDataServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WnRatingDataServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wagdynavas/wnratingdataservice/WnRatingDataServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wagdynavas.wnratingdataservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WnRatingDataServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
